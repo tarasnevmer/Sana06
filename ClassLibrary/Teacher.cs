2fc58dca8e337c69275e095b9ec735c65f4e600a
Taras Nevmerzhytskyi <kn222_ntv@student.ztu.edu.ua>
2024-01-10 12:27:50 +0200

    Створено та реалізовано клас Teacher (Викладач)

---
+++ ClassLibrary/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace ClassLibrary
+{
+    public class Teacher : Person
+    {
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string EducationInstitution { get; set; }
+
+        public Teacher(string firstName, string lastName, 
+            DateTime birthDate, string position, 
+            string department, string educationInstitution) : base (firstName, lastName, birthDate)
+        {
+            Position = position;
+            Department = department;
+            EducationInstitution = educationInstitution;
+        }
+    }
+}
