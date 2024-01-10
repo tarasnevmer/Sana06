8de4d51d078a26328fc13a9efd1b27bd7c6818ee
Taras Nevmerzhytskyi <kn222_ntv@student.ztu.edu.ua>
2024-01-10 12:21:35 +0200

    Створено та реалізовано клас Student (Студент)

---
+++ ClassLibrary/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClassLibrary
+{
+    public class Student : Person
+    {
+        public int Course { get; set; }
+        public string Group { get; set; }
+        public string Faculty { get; set; }
+        public string EducationInstitution { get; set; }
+
+        public Student(string firstName, string lastName, DateTime birthDate, int course, 
+            string group, string faculty, string educationInstitution) : base (firstName, lastName, birthDate)
+        {
+            Course = course;
+            Group = group;
+            Faculty = faculty;
+            EducationInstitution = educationInstitution;
+        }
+    }
+}
