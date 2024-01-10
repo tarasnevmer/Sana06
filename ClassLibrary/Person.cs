2635bb59ed74e4d6ea675e93f7480494fcab8164
Taras Nevmerzhytskyi <kn222_ntv@student.ztu.edu.ua>
2024-01-10 12:04:01 +0200

    Створено та реалізовано клас Person

---
+++ ClassLibrary/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Person (string firstName, string lastName, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+    }
+}
